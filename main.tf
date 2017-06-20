7ad82ac54830e65fa039f84f5f380225b11e1358
Mo Tahir <motahir@users.noreply.github.com>
2017-06-20 22:12:44 +0100

    Add env variable (#1)
    * Refactored to add env variable and tidy up
    
    * formatting

--- main.tf
+++ main.tf
@@ -5,21 +5,17 @@ module "vnet" {
   address_space        = "${var.address_space}"
   address_prefixes     = "${var.address_prefixes}"
   subnetinstance_count = "${var.subnetinstance_count}"
-  tag                  = "${var.tag}"
+  env                  = "${var.env}"
 }
 
-module "azurerm_app_service_environment_ilb" {
-  source                       = "git@github.com:contino/moj-module-ase.git"
-  tag                          = "${var.tag}"
-  name                         = "${var.name}"
-  stagingslotname              = "${var.stagingslotname}"
-  lastknowngoodslotname        = "${var.lastknowngoodslotname}"
-  location                     = "${var.location}"
-  vnetresourceid               = "${module.vnet.id}"
-  subnetname                   = "${module.vnet.subnet_names[1]}"
-  frontend_size                = "${var.frontend_size}"
-  workerpoolone_instancesize   = "${var.workerpoolone_instancesize}"
-  workerpooltwo_instancesize   = "${var.workerpooltwo_instancesize}"
-  workerpoolthree_instancesize = "${var.workerpoolthree_instancesize}"
-  resourcegroupname            = "${module.vnet.resourcegroup_name}"
+module "azurerm_app_service_environment" {
+  source                = "git@github.com:contino/moj-module-ase.git"
+  name                  = "${var.name}"
+  stagingslotname       = "${var.stagingslotname}"
+  lastknowngoodslotname = "${var.lastknowngoodslotname}"
+  location              = "${var.location}"
+  vnetresourceid        = "${module.vnet.id}"
+  subnetname            = "${module.vnet.subnet_names[1]}"
+  resourcegroupname     = "${module.vnet.resourcegroup_name}"
+  env                   = "${var.env}"
 }
--- variables.tf
+++ variables.tf
@@ -34,27 +34,7 @@ variable "address_prefixes" {
   default = ["192.168.0.0/24", "192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24", "192.168.4.0/24"]
 }
 
-variable "frontend_size" {
-  type    = "string"
-  default = "Medium"
-}
-
-variable "workerpoolone_instancesize" {
-  type    = "string"
-  default = "Small"
-}
-
-variable "workerpooltwo_instancesize" {
-  type    = "string"
-  default = "Small"
-}
-
-variable "workerpoolthree_instancesize" {
-  type    = "string"
-  default = "Small"
-}
-
-variable "tag" {
+variable "env" {
   type    = "string"
   default = "local"
 }
