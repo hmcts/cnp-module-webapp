abcb00cb7698a6281a197b3df143e32a8cf321ea
sanabriad <sanabria.d@gmail.com>
2017-07-23 20:47:57 +0100

    Adjusting controls to random_name

--- tests/int/test/integration/default/controls/resource_groups.rb
+++ tests/int/test/integration/default/controls/resource_groups.rb
@@ -2,14 +2,16 @@
 # copyright: 2017, The Authors
 # license: All rights reserved
 
-#title 'Check Azure Resource Group Configuration'
-#
-#control 'azure-resource-groups' do
-#
-#  impact 1.0
-#  title ' Check that the resource group exist'
-#
-#  describe azure_resource_group(name: 'inspect-frontend-int') do
-#    its('location') { should eq 'uksouth' }
-#  end
-#end
+title 'Check Azure Resource Group Configuration'
+
+control 'azure-resource-groups' do
+
+  impact 1.0
+  title ' Check that the resource group exist'
+  json_obj = json('.kitchen/kitchen-terraform/default-azure/terraform.tfstate')
+  random_name = json_obj['modules'][0]['outputs']['random_name']['value'] + '-frontend-int'
+
+  describe azure_resource_group(name: random_name) do
+    its('location') { should eq 'uksouth' }
+  end
+end
--- tests/int/test/integration/default/controls/service_plans.rb
+++ tests/int/test/integration/default/controls/service_plans.rb
@@ -4,7 +4,7 @@ control 'azure-service-plan' do
 
   impact 1.0
   title 'Check that the service has the correct properties'
-  json_obj = json('.kitchen/kitchen-terraform/default/terraform.tfstate')
+  json_obj = json('.kitchen/kitchen-terraform/default-azure/terraform.tfstate')
   random_name = json_obj['modules'][0]['outputs']['random_name']['value'] + '-frontend-int'
 
 
@@ -13,7 +13,7 @@ control 'azure-service-plan' do
     its('location') { should eq 'UK South' }
     its('name') { should eq "#{random_name}" }
     its('ase_name') { should eq 'sandbox-core-infra' }
-    its('maximum_number_of_workers') { should eq 4 }
+    its('maximum_number_of_workers') { should eq 3 }
     its('number_of_sites') { should eq 1 }
   end
 end
