516512d19c2e7d682f0fce9556c63da41705ebc2
Daniel Sanabria <sanabria.d@gmail.com>
2017-07-23 15:07:19 +0100

    Debugging controls

--- tests/int/test/integration/default/controls/resource_groups.rb
+++ tests/int/test/integration/default/controls/resource_groups.rb
@@ -2,14 +2,14 @@
 # copyright: 2017, The Authors
 # license: All rights reserved
 
-title 'Check Azure Resource Group Configuration'
-
-control 'azure-resource-groups' do
-
-  impact 1.0
-  title ' Check that the resource group exist'
-
-  describe azure_resource_group(name: 'inspect-frontend-int') do
-    its('location') { should eq 'uksouth' }
-  end
-end
+#title 'Check Azure Resource Group Configuration'
+#
+#control 'azure-resource-groups' do
+#
+#  impact 1.0
+#  title ' Check that the resource group exist'
+#
+#  describe azure_resource_group(name: 'inspect-frontend-int') do
+#    its('location') { should eq 'uksouth' }
+#  end
+#end
