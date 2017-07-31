2fbc2340cf2d86a31eaabf6954d55e995dfee6c6
mtahir <mo.tahir@contino.io>
2017-07-31 17:52:17 +0100

    Updated test fixture

--- tests/int/test/fixtures/tf/moj_azure_fixtures.tf
+++ tests/int/test/fixtures/tf/moj_azure_fixtures.tf
@@ -14,6 +14,10 @@ variable "env" {
   default = "int"
 }
 
+variable "app_settings" {
+  default = "${jsonencode(map("TEST_SETTING", "Setting"))}"
+}
+
 data "terraform_remote_state" "core_sandbox_infrastructure" {
   backend = "azure"
 
@@ -26,11 +30,12 @@ data "terraform_remote_state" "core_sandbox_infrastructure" {
 }
 
 module "frontend" {
-  source   = "git::https://7fed81743d89f663cc1e746f147c83a74e7b1318@github.com/contino/moj-module-webapp?ref=0.0.16"
-  product  = "${var.random_name}-frontend"
-  location = "${var.location}"
-  env      = "${var.env}"
-  asename  = "${data.terraform_remote_state.core_sandbox_infrastructure.ase_name[0]}"
+  source       = "git::https://7fed81743d89f663cc1e746f147c83a74e7b1318@github.com/contino/moj-module-webapp?ref=0.0.25"
+  product      = "${var.random_name}-frontend"
+  location     = "${var.location}"
+  env          = "${var.env}"
+  asename      = "${data.terraform_remote_state.core_sandbox_infrastructure.ase_name[0]}"
+  app_settings = "${var.app_settings}"
 }
 
 output "random_name" {
