caab85f6fe060c364817c992ba850a91b6bb4b0a
mtahir <mo.tahir@contino.io>
2017-08-17 16:37:57 +0100

    Added new outputs

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "webapp_name" {
   value = "${azurerm_template_deployment.app_service_site.name}"
 }
+
+output "gitendpoint" {
+  value = "${azurerm_template_deployment.app_service_site.name}.scm.${var.asename}.p.azurewebsites.net/${azurerm_template_deployment.app_service_site.name}.git"
+}
+
+output "url" {
+  value = "http://${azurerm_template_deployment.app_service_site.name}.${var.asename}.p.azurewebsites.net"
+}
