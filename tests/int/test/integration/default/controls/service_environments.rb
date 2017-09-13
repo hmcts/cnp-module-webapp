81eead26a3f694f8c86617c4be8f334f53bf33f0
Hibri Marzook <hibri@hibri.net>
2017-09-13 16:07:50 +0100

    fix names in expected resource groups

--- tests/int/test/integration/default/controls/service_environments.rb
+++ tests/int/test/integration/default/controls/service_environments.rb
@@ -8,11 +8,11 @@ control 'azure-service-environment' do
   # TODO: Because of the limitations for ASEv1 we use an existing ASE
   # to validate our lib
   # Ensure that the expected resources have been deployed
-  describe azure_service_environment(rg_name: 'sandbox-core-infra', name: 'sandbox-core-infra') do
+  describe azure_service_environment(rg_name: 'sandbox-core-infra-dev', name: 'sandbox-core-infra') do
     its('location') { should eq 'UK South' }
     its('name') { should eq 'sandbox-core-infra' }
     its('vnet_name') { should eq 'sandbox-core-infra-vnet' }
-    its('vnet_resource_group_name') { should eq 'sandbox-core-infra' }
+    its('vnet_resource_group_name') { should eq 'sandbox-core-infra-dev' }
     its('vnet_subnet_name') { should eq 'sandbox-core-infra-subnet-0' }
     its('internal_load_balancing_mode') { should eq 'None' }
     its('maximum_number_of_machines') { should eq 55 }
