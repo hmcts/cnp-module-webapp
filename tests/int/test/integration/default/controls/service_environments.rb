1c9c2ffe972a4a961c9d1b5b4d700a835fabb2a5
Hibri Marzook <hibri@hibri.net>
2017-10-03 12:54:04 +0100

    change default scale to I2 (#24)
    * change default scale to I2
    
    * use the correct sandbox
    
    * change names to sandbox-core-infra
    
    * change more names
    
    * change to use subnet 0

--- tests/int/test/integration/default/controls/service_environments.rb
+++ tests/int/test/integration/default/controls/service_environments.rb
@@ -8,12 +8,12 @@ control 'azure-service-environment' do
   # TODO: Because of the limitations for ASEv1 we use an existing ASE
   # to validate our lib
   # Ensure that the expected resources have been deployed
-  describe azure_service_environment(rg_name: 'sandbox-core-infra-dev', name: 'sandbox-core-compute-dev') do
+  describe azure_service_environment(rg_name: 'sandbox-core-infra-dev', name: 'sandbox-core-infra-dev') do
     its('location') { should eq 'UK South' }
-    its('name') { should eq 'sandbox-core-compute-dev' }
+    its('name') { should eq 'sandbox-core-infra-dev' }
     its('vnet_name') { should eq 'sandbox-core-infra-vnet-dev' }
     its('vnet_resource_group_name') { should eq 'sandbox-core-infra-dev' }
-    its('vnet_subnet_name') { should eq 'sandbox-core-infra-subnet-3-dev' }
+    its('vnet_subnet_name') { should eq 'sandbox-core-infra-subnet-0-dev' }
     its('internal_load_balancing_mode') { should eq 'None' }
     its('maximum_number_of_machines') { should eq 250 }
   end
