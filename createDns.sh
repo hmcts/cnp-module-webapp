209aed6de83c08b3fef4ed2e8fb406906e85a908
James Whinn <jameswhinn89@gmail.com>
2017-11-08 18:32:33 +0000

    rm exec

--- createDns.sh
+++ createDns.sh
@@ -29,7 +29,7 @@ sed -i -e "s/aseIlb/$consul/g" "${uri}/consul.json"
 curl --request PUT --data $uri/consul.json "http://${consul}:8500/v1/agent/service/register"
 
 sed -i -e "s/$domain/scm/g" "${uri}/consul.json"
-sed -i -e "s/\[\]/\[$domain\]/g" "${uri}/consul.json"
+sed -i -e "s/\[\]/\[\"$domain\"\]/g" "${uri}/consul.json"
 
 
 curl --request PUT --data $uri/consul.json "http://${consul}:8500/v1/agent/service/register"
