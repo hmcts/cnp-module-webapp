931ecc725c1d65bcab108d0cc4b9b7eeb15f4220
James Whinn <jameswhinn89@gmail.com>
2017-11-08 17:51:57 +0000

    add consul conf

--- createDns.sh
+++ createDns.sh
@@ -13,13 +13,13 @@ consul=$(echo "$consul" | sed -e 's/^"//' -e 's/"$//')
 echo $consul
 
 # Create 2 consul entries (service + scm)
-sed -i -e "s/serviceId/$domain/g" $uri/consul.json
-sed -i -e "s/serviceName/$domain/g" $uri/consul.json
-sed -i -e "s/aseIlb/$consul/g" $uri/consul.json
+sed -i -e "s/serviceId/$domain/g" "${uri}/consul.json"
+sed -i -e "s/serviceName/$domain/g" "${uri}/consul.json"
+sed -i -e "s/aseIlb/$consul/g" "${uri}/consul.json"
 
 curl --request PUT --data @$uri/consul.json http://$consul:8500/v1/agent/service/register
 
-sed -i -e "s/$domain/scm/g" $uri/consul.json
-sed -i -e "s/\[\]/\[$domain\]/g" $uri/consul.json
+sed -i -e "s/$domain/scm/g" "${uri}/consul.json"
+sed -i -e "s/\[\]/\[$domain\]/g" "${uri}/consul.json"
 
-curl --request PUT --data @$uri/consul.json http://$consul:8500/v1/agent/service/register
+curl --request PUT --data @"${uri}/consul.json" http://$consul:8500/v1/agent/service/register
