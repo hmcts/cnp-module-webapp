f7bea3862045d5472eacd6bfbd52a3ed5c5a5246
James Whinn <jameswhinn89@gmail.com>
2017-11-08 18:13:02 +0000

    rm exec

--- createDns.sh
+++ createDns.sh
@@ -5,6 +5,10 @@ domain=$1
 rg=$2
 uri=$3
 
+echo $domain
+echo $rg
+echo $uri
+
 #get ip of consul
 consul=$(az vmss nic list --resource-group $rg --vmss-name consul-server --query "[0].ipConfigurations[0].privateIpAddress")
 
@@ -17,9 +21,9 @@ sed -i -e "s/serviceId/$domain/g" "${uri}/consul.json"
 sed -i -e "s/serviceName/$domain/g" "${uri}/consul.json"
 sed -i -e "s/aseIlb/$consul/g" "${uri}/consul.json"
 
-curl --request PUT --data @"{$uri}/consul.json" "http://${consul}:8500/v1/agent/service/register"
+curl --request PUT --data "{$uri}/consul.json" "http://${consul}:8500/v1/agent/service/register"
 
 sed -i -e "s/$domain/scm/g" "${uri}/consul.json"
 sed -i -e "s/\[\]/\[$domain\]/g" "${uri}/consul.json"
 
-curl --request PUT --data @"${uri}/consul.json" "http://${consul}:8500/v1/agent/service/register"
+curl --request PUT --data "${uri}/consul.json" "http://${consul}:8500/v1/agent/service/register"
