d76309dcb141c6e9d27372fe32f5cd37b048b5d9
James Whinn <jameswhinn89@gmail.com>
2017-11-08 16:37:27 +0000

    add local exec for dns creation

---
+++ createDns.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+rg=$1
+domain=$1
+
+
+#get ip of consul
+consul=$(az vmss nic list --resource-group $rg --vmss-name consul-server --query "[0].ipConfigurations[0].privateIpAddress")
+
+# Create 2 consul entries (service + scm)
+sed -i -e "s/serviceId/$domain/g" consul.json
+sed -i -e "s/serviceName/$domain/g" consul.json
+sed -i -e "s/aseIlb/$consul/g" consul.json
+
+curl --request PUT --data @consul.json http://$consul:8500/v1/agent/service/register
+
+sed -i -e "s/$domain/scm/g" consul.json
+sed -i -e "s/\[\]/\[$domain\]/g" consul.json
+
+curl --request PUT --data @consul.json http://$consul:8500/v1/agent/service/register
--- main.tf
+++ main.tf
@@ -11,19 +11,29 @@ data "template_file" "sitetemplate" {
 
 # Create Application Service site
 resource "azurerm_template_deployment" "app_service_site" {
-  template_body       = "${data.template_file.sitetemplate.rendered}"
-  name                = "${var.product}-${var.env}"
+  template_body = "${data.template_file.sitetemplate.rendered}"
+  name = "${var.product}-${var.env}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
-  deployment_mode     = "Incremental"
+  deployment_mode = "Incremental"
 
   parameters = {
-    name               = "${var.product}-${var.env}"
-    location           = "${var.location}"
-    env                = "${var.env}"
-    app_settings       = "${jsonencode(merge(var.app_settings_defaults, var.app_settings))}"
-    certificateName    = "${var.product}-${var.env}"
-    hostname           = "${var.product}-${var.env}.service.internal"
+    name = "${var.product}-${var.env}"
+    location = "${var.location}"
+    env = "${var.env}"
+    app_settings = "${jsonencode(merge(var.app_settings_defaults, var.app_settings))}"
+    certificateName = "webapp"
+    hostname = "${var.product}-${var.env}.service.consul"
     sslVaultSecretName = "${var.product}-${var.env}"
-    key_vault_id       = "${var.key_vault_id}"
+    key_vault_id = "${var.key_vault_id}"
+  }
+}
+
+resource "null_resource" "consul" {
+  triggers {
+    trigger = "${azurerm_template_deployment.app_service_site.name}"
+  }
+
+  provisioner "local-exec" {
+    command = "bash -e ./createDns.sh ${var.product}-${var.env}"
   }
 }
