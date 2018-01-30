a0a964a40a09cf8b7eb13035163e94885edc1ecd
Neil Richardson <960806+njrich28@users.noreply.github.com>
2018-01-30 11:22:52 +0000

    Add registration of slot DNS name (#45)

--- main.tf
+++ main.tf
@@ -31,7 +31,13 @@ resource "null_resource" "consul" {
     trigger = "${azurerm_template_deployment.app_service_site.name}"
   }
 
+  # register 'production' slot dns
   provisioner "local-exec" {
     command = "bash -e ${path.module}/createDns.sh '${var.product}-${var.env}' 'core-infra-${var.env}' '${path.module}' '${var.ilbIp}'"
+  }
+
+  # register 'staging' slot dns
+  provisioner "local-exec" {
+    command = "bash -e ${path.module}/createDns.sh '${var.product}-${var.env}-${var.staging_slot_name}' 'core-infra-${var.env}' '${path.module}' '${var.ilbIp}'"
   }
 }
