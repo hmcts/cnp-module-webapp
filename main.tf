b2330ddae72378a7933fe02af79b040f03a63a1c
leszekg <gonczar@gmail.com>
2018-02-09 15:06:15 +0000

    Allow for custom resource group names

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
+locals {
+  default_resource_group_name = "${var.product}-${var.env}"
+  resource_group_name = "${var.resource_group_name != "" ? var.resource_group_name : local.default_resource_group_name}"
+}
+
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.env}"
+  name     = "${local.resource_group_name}"
   location = "${var.location}"
 }
 
