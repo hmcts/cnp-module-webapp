a09b8f265b1ce086aa231d1e027ff36edfc95a41
James Whinn <jameswhinn89@gmail.com>
2018-03-14 21:05:19 +0000

    add logic for maintenance page

--- trafficman.tf
+++ trafficman.tf
@@ -40,10 +40,29 @@ resource "azurerm_traffic_manager_endpoint" "backend" {
   weight              = 100
 }
 
+resource "azurerm_storage_account" "sa" {
+  name                     = "${var.product}-${var.env}"
+  resource_group_name      = "${azurerm_resource_group.rg.name}"
+  location                 = "${var.location}"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags {
+    environment = "${var.env}"
+  }
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "${var.product}-${var.env}-maintenance"
+  resource_group_name   = "${azurerm_resource_group.rg.name}"
+  storage_account_name  = "${azurerm_storage_account.sa.name}"
+  container_access_type = "blob"
+}
+
 resource "azurerm_storage_blob" "maintenance" {
   count                  = "${var.include_maintenance}"
   name                   = "${var.product}-${var.env}-maintenance"
-  resource_group_name    = ""
-  storage_account_name   = ""
-  storage_container_name = ""
+  resource_group_name    = "${azurerm_resource_group.rg.name}"
+  storage_account_name   = "${azurerm_storage_account.sa.name}"
+  storage_container_name = "${azurerm_storage_container.container.name}"
 }
