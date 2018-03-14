cdf19456cce10c424625ba97d0cb16f9a0be8e88
James Whinn <jameswhinn89@gmail.com>
2018-03-14 21:22:57 +0000

    add logic for maintenance page

--- trafficman.tf
+++ trafficman.tf
@@ -41,7 +41,7 @@ resource "azurerm_traffic_manager_endpoint" "backend" {
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                     = "${replace(${var.product},-,)${var.env}}"
+  name                     = "${replace(${var.product},-,)}${var.env}"
   resource_group_name      = "${azurerm_resource_group.rg.name}"
   location                 = "${var.location}"
   account_tier             = "Standard"
