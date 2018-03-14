52b882ce6c09addb82ace47802fd8737aa2323ca
James Whinn <jameswhinn89@gmail.com>
2018-03-14 17:36:27 +0000

    fix ref

--- trafficman.tf
+++ trafficman.tf
@@ -2,6 +2,6 @@ module "trafficmanager" {
   source              = "git::git@github.com:contino/moj-module-trafficmanager?ref=master"
   name                = "${var.product}"
   env                 = "${var.env}"
-  resource_group_name = "${var.resource_group_name}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
   app_gw_id           = "${azurerm_application_gateway.waf.id}"
 }
