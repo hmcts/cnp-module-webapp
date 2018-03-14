5d89824dc30ad3b0fe9d84e747aba79106d73496
James Whinn <jameswhinn89@gmail.com>
2018-03-14 20:01:30 +0000

    rm traffic man

--- trafficman.tf
+++ trafficman.tf
@@ -6,33 +6,31 @@
 #   resource_group_name = "${azurerm_resource_group.rg.name}"
 #   app_gw_id           = "${azurerm_application_gateway.waf.id}"
 # }
+// perhaps this should belong at core-infra level then only backend configuration here
+# resource "azurerm_traffic_manager_profile" "trafficmanager" {
+#   name                   = "${var.product}-${var.env}"
+#   resource_group_name    = "${azurerm_resource_group.rg.name}"
+#   traffic_routing_method = "Weighted"
+#   dns_config {
+#     relative_name = "hmcts-${var.env}"
+#     ttl           = 100
+#   }
+#   monitor_config {
+#     protocol = "http"
+#     port     = 80
+#     path     = "/"
+#   }
+#   tags {
+#     environment = "${var.env}"
+#   }
+# }
+# // Add multi backend logic here 
+# resource "azurerm_traffic_manager_endpoint" "backend" {
+#   name                = "${var.product}-${var.env}"
+#   resource_group_name = "${azurerm_resource_group.rg.name}"
+#   profile_name        = "${azurerm_traffic_manager_profile.trafficmanager.name}"
+#   target              = "${azurerm_public_ip.appGwPIP.fqdn}"
+#   type                = "externalEndpoints"
+#   weight              = 100
+# }
 
-resource "azurerm_traffic_manager_profile" "trafficmanager" {
-  name                   = "${var.product}-${var.env}"
-  resource_group_name    = "${azurerm_resource_group.rg.name}"
-  traffic_routing_method = "Weighted"
-
-  dns_config {
-    relative_name = "hmcts-${var.env}"
-    ttl           = 100
-  }
-
-  monitor_config {
-    protocol = "http"
-    port     = 80
-    path     = "/"
-  }
-
-  tags {
-    environment = "${var.env}"
-  }
-}
-
-resource "azurerm_traffic_manager_endpoint" "backend" {
-  name                = "${var.product}-${var.env}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  profile_name        = "${azurerm_traffic_manager_profile.trafficmanager.name}"
-  target              = "${azurerm_public_ip.appGwPIP.fqdn}"
-  type                = "externalEndpoints"
-  weight              = 100
-}
