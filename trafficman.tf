7982fa61d5ff34b0c3c41877060cf6f508f0d6d3
James Whinn <jameswhinn89@gmail.com>
2018-03-21 14:19:48 +0000

    del resource

--- trafficman.tf
+++ trafficman.tf
@@ -1,19 +1,16 @@
-data "template_file" "tmtemplate" {
-  template = "${file("${path.module}/templates/trafficmanager.json")}"
-}
-
-resource "azurerm_template_deployment" "tmprofile" {
-  template_body       = "${data.template_file.tmtemplate.rendered}"
-  name                = "${var.product}-${var.env}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  deployment_mode     = "Incremental"
-
-  parameters = {
-    name                 = "${var.product}-${var.env}"
-    additional_host_name = "${var.additional_host_name}"
-  }
-}
-
+# data "template_file" "tmtemplate" {
+#   template = "${file("${path.module}/templates/trafficmanager.json")}"
+# }
+# resource "azurerm_template_deployment" "tmprofile" {
+#   template_body       = "${data.template_file.tmtemplate.rendered}"
+#   name                = "${var.product}-${var.env}"
+#   resource_group_name = "${azurerm_resource_group.rg.name}"
+#   deployment_mode     = "Incremental"
+#   parameters = {
+#     name                 = "${var.product}-${var.env}"
+#     additional_host_name = "${var.additional_host_name}"
+#   }
+# }
 # // Add multi backend logic for mult az here later
 # resource "azurerm_traffic_manager_endpoint" "backend" {
 #   count               = "${var.is_frontend}"
