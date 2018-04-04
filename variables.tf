15e42ad3219d7fe8b82450d0d319fe02598b9f2d
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-04-04 12:14:27 +0100

    Remove a default for testing

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "appinsights_location" {
 
 variable "appinsights_instrumentation_key" {
   description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
-  default = ""
+  # default = ""
 }
 
 variable "env" {
