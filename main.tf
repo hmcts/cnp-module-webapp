3059bb20ad118323c1130fe4b237ef9ebf82ce10
Louis Houghton <louis@louishoughton.com>
2018-05-04 13:33:42 +0100

    Expose webSocketsEnabled Azure setting (#97)
    * Add configuration for enabling websockets
    
    * Add webSocketsEnabled parameter
    
    * Add parameter with default
    
    * Remove trailing comma
    
    * Add config to sites area
    
    * Snake case

--- main.tf
+++ main.tf
@@ -59,6 +59,7 @@ resource "azurerm_template_deployment" "app_service_site" {
     https_only           = "${var.https_only}"
     capacity             = "${var.capacity}"
     is_frontend          = "${var.is_frontend}"
+    web_sockets_enabled  = "${var.web_sockets_enabled}"
     asp_name             = "${var.asp_name}-${var.env}"
   }
 }
