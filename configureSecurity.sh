7583b19bc199b05ac127462b16fb2324d4e53b6c
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2018-09-06 15:54:59 +0100

    Update configureSecurity.sh

--- configureSecurity.sh
+++ configureSecurity.sh
@@ -6,14 +6,13 @@ tenantId="https://sts.windows.net/$4"
 clientId=$5
 clientSecret=$6
 loginAction=LoginWithAzureActiveDirectory
-env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription
 
 if [ ! -z "$tenantId" ] && [ ! -z "$clientId" ] && [ ! -z "$clientSecret" ];
 then
-	az webapp auth update --name $appName --resource-group $resourceGroup --aad-token-issuer-url $tenantId --aad-client-id $clientId --action $loginAction --aad-client-secret $clientSecret --enabled true
+	env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az webapp auth update --name $appName --resource-group $resourceGroup --aad-token-issuer-url $tenantId --aad-client-id $clientId --action $loginAction --aad-client-secret $clientSecret --enabled true
 elif [ ! -z "$tenantId" ] && [ ! -z "$clientId" ];
 then
-	az webapp auth update --name $appName --resource-group $resourceGroup --aad-token-issuer-url $tenantId --aad-client-id $clientId --action $loginAction --enabled true
+	env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az webapp auth update --name $appName --resource-group $resourceGroup --aad-token-issuer-url $tenantId --aad-client-id $clientId --action $loginAction --enabled true
 else
-	az webapp auth update --name $appName --resource-group $resourceGroup --enabled false
+	env AZURE_CONFIG_DIR=/opt/jenkins/.azure-$subscription az webapp auth update --name $appName --resource-group $resourceGroup --enabled false
 fi
