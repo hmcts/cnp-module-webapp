6f6143cad0829691627d6e9c77d0391b9b909a29
Daniel Sanabria <sanabria.d@gmail.com>
2018-09-07 15:42:33 +0100

    Adding tags for native resources

--- main.tf
+++ main.tf
@@ -36,6 +36,10 @@ resource "azurerm_application_insights" "appinsights" {
   location            = "${var.appinsights_location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   application_type    = "${var.application_type}"
+
+  tags = "${merge(var.common_tags,
+    map("lastUpdated", "${timestamp()}")
+    )}"
 }
 
 locals {
