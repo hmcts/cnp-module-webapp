5f787da560ba687b92b157d6810f005e544a7d78
Henry Dobson <henrydobson@me.com>
2019-10-31 10:21:17 +0000

    refactor(asp settings): add ARM parameter for client_affinity (#157)

--- examples/main.tf
+++ examples/main.tf
@@ -1,7 +1,8 @@
 provider "azurerm" {
-  version = "1.22.1"
+  version         = "1.22.1"
   subscription_id = "${var.subscription_id}"
 }
+
 variable "subscription" {}
 
 variable "subscription_id" {}
@@ -22,7 +23,6 @@ variable "capacity" {
   default = "1"
 }
 
-
 variable "common_tags" {
   default = {
     "Team Name" = "IDAM"
@@ -35,40 +35,40 @@ variable "common_tags" {
 # parameter, which will work for default_asp_name_and_rg but fail on
 # params_asp_name_and_rg.
 module "default_asp_name_and_rg" {
-	source               = "../"
-	product              = "${var.product}-frontend-example"
-	location             = "${var.location}"
-	appinsights_location = "${var.location}"
-	env                  = "${var.env}"
-	capacity             = "${var.capacity}"
-	is_frontend          = true
-	asp_name             = "${var.product}-${var.env}"
-	subscription         = "${var.subscription}"
-	common_tags          = "${var.common_tags}"
-	
-	# asp_name = "idam-web-frontend-example-sandbox"
+  source               = "../"
+  product              = "${var.product}-frontend-example"
+  location             = "${var.location}"
+  appinsights_location = "${var.location}"
+  env                  = "${var.env}"
+  capacity             = "${var.capacity}"
+  is_frontend          = true
+  asp_name             = "${var.product}-${var.env}"
+  subscription         = "${var.subscription}"
+  common_tags          = "${var.common_tags}"
+
+  # asp_name = "idam-web-frontend-example-sandbox"
   # asp_rg = "idam-web-frontend-example-sandbox"
 
-	app_settings         = {
-		WEBSITE_NODE_DEFAULT_VERSION = "8.8.0"
-	}
+  app_settings = {
+    WEBSITE_NODE_DEFAULT_VERSION = "8.8.0"
+  }
 }
 
 module "params_asp_name_and_rg" {
-	source               = "../"
-	product              = "${var.product}-backend-example"
-	location             = "${var.location}"
-	appinsights_location = "${var.location}"
-	env                  = "${var.env}"
-	capacity             = "${var.capacity}"
-	asp_name             = "${var.product}-${var.env}"
-	subscription         = "${var.subscription}"
-	common_tags          = "${var.common_tags}"
-	
-	asp_name = "idam-backend-example-sandbox"
-  asp_rg = "idam-backend-example-sandbox"
+  source               = "../"
+  product              = "${var.product}-backend-example"
+  location             = "${var.location}"
+  appinsights_location = "${var.location}"
+  env                  = "${var.env}"
+  capacity             = "${var.capacity}"
+  asp_name             = "${var.product}-${var.env}"
+  subscription         = "${var.subscription}"
+  common_tags          = "${var.common_tags}"
 
-	app_settings         = {
-		WEBSITE_NODE_DEFAULT_VERSION = "8.8.0"
-	}
+  asp_name = "idam-backend-example-sandbox"
+  asp_rg   = "idam-backend-example-sandbox"
+
+  app_settings = {
+    WEBSITE_NODE_DEFAULT_VERSION = "8.8.0"
+  }
 }
