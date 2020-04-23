f4713d34d9e240689efbf83360a2deeb378701d5
Tim Jacomb <tim.jacomb@hmcts.net>
2020-04-23 15:52:50 +0100

    Fix app insights error (#163)

--- variables.tf
+++ variables.tf
@@ -63,7 +63,7 @@ variable "resource_group_name" {
 
 variable "application_type" {
   type        = "string"
-  default     = "Web"
+  default     = "web"
   description = "Type of Application Insights (Web/Other)"
 }
 
