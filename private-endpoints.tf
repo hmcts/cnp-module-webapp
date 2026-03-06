71a04cdc8794bcf4f0da8ac99bd90a3eba724efd
Ieuan Byers <ieuan.byers@justice.gov.uk>
2026-03-06 15:24:13 +0000

    Fix private endpoints

--- private-endpoints.tf
+++ private-endpoints.tf
@@ -1,5 +1,21 @@
-resource "azurerm_private_endpoint" "webapp_private_endpoint" {
-  count = var.private_endpoint_enabled ? 1 : 0
+resource "azurerm_private_endpoint" "linux_webapp_private_endpoint" {
+  count = var.os_type == "linux" && var.private_endpoint_enabled ? 1 : 0
+
+  name                = "${var.webapp_name}-pe"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  subnet_id           = var.private_endpoint_subnet_id
+
+  private_service_connection {
+    name                           = "${var.webapp_name}-psc"
+    is_manual_connection           = false
+    private_connection_resource_id = azurerm_linux_web_app.linux_web_app[0].id
+    subresource_names              = ["sites"]
+  }
+}
+
+resource "azurerm_private_endpoint" "windows_webapp_private_endpoint" {
+  count = var.os_type == "windows" && var.private_endpoint_enabled ? 1 : 0
 
   name                = "${var.webapp_name}-pe"
   resource_group_name = var.resource_group_name
@@ -12,5 +28,4 @@ resource "azurerm_private_endpoint" "webapp_private_endpoint" {
     private_connection_resource_id = azurerm_windows_web_app.windows_web_app[0].id
     subresource_names              = ["sites"]
   }
-
 }
