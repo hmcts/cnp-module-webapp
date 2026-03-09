3390964ca1c7b1018d67b65c28d76cc0dd7bbf93
Ieuan Byers <ieuan.byers@justice.gov.uk>
2026-03-09 08:55:26 +0000

    Fix resource name

--- private-endpoints.tf
+++ private-endpoints.tf
@@ -1,13 +1,13 @@
 resource "azurerm_private_endpoint" "linux_webapp_private_endpoint" {
   count = var.os_type == "linux" && var.private_endpoint_enabled ? 1 : 0
 
-  name                = "${var.webapp_name}-pe"
+  name                = "${local.effective_webapp_name}-pe"
   resource_group_name = var.resource_group_name
   location            = var.location
   subnet_id           = var.private_endpoint_subnet_id
 
   private_service_connection {
-    name                           = "${var.webapp_name}-psc"
+    name                           = "${local.effective_webapp_name}-psc"
     is_manual_connection           = false
     private_connection_resource_id = azurerm_linux_web_app.linux_web_app[0].id
     subresource_names              = ["sites"]
@@ -17,13 +17,13 @@ resource "azurerm_private_endpoint" "linux_webapp_private_endpoint" {
 resource "azurerm_private_endpoint" "windows_webapp_private_endpoint" {
   count = var.os_type == "windows" && var.private_endpoint_enabled ? 1 : 0
 
-  name                = "${var.webapp_name}-pe"
+  name                = "${local.effective_webapp_name}-pe"
   resource_group_name = var.resource_group_name
   location            = var.location
   subnet_id           = var.private_endpoint_subnet_id
 
   private_service_connection {
-    name                           = "${var.webapp_name}-psc"
+    name                           = "${local.effective_webapp_name}-psc"
     is_manual_connection           = false
     private_connection_resource_id = azurerm_windows_web_app.windows_web_app[0].id
     subresource_names              = ["sites"]
